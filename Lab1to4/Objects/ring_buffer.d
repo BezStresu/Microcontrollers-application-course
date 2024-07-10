3f13b411f3476d5a3c3bf9a1c6f47c86f28e16fc
BezStresu <102317065+BezStresu@users.noreply.github.com>
2024-07-10 02:45:01 +0200

    Add files via upload

---
+++ Lab1to4/Objects/ring_buffer.d
@@ -0,0 +1,7 @@
+./objects/ring_buffer.o: ring_buffer.c RTE\_deafult\Pre_Include_Global.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\assert.h ring_buffer.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\stdint.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\stdbool.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\stddef.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\stdlib.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\string.h
---
+++ Lab1to4/Objects/usart_unit_test.d
@@ -0,0 +1,5 @@
+./objects/usart_unit_test.o: usart_unit_test.c \
+  RTE\_deafult\Pre_Include_Global.h usart.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\stdint.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\stdbool.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\stddef.h
